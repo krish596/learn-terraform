8f7efa42b372b416441d9571f3a297807df101a5
Chimtamreddy <kr7348202@gmail.com>
2024-08-06 22:12:47 +0530

    roboshop-shellscript

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t3.micro"
-  vpc_security_group_ids = "[sg-0402859183bbbfa7d]"
+  vpc_security_group_id = [sg-0402859183bbbfa7d]
 
   tags = {
     Name = "frontend"
