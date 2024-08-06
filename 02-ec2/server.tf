0b5d2edc1c051a76ea6cc28c3135eb4cd891567d
Chimtamreddy <kr7348202@gmail.com>
2024-08-06 22:06:36 +0530

    roboshop-shellscript

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,8 +1,9 @@
-resource "aws_instance" "instance" {
+resource "aws_instance" "frontend" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "frontend"
   }
 }
+
