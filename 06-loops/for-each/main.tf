eda690e91749d0c3194624f94d23bbcfb92afca6
Chimtamreddy <kr7348202@gmail.com>
2024-08-08 22:11:07 +0530

    Terraform

--- 06-loops/for-each/main.tf
+++ 06-loops/for-each/main.tf
@@ -14,4 +14,9 @@ resource "aws_instance" "instance" {
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0402859183bbbfa7d"]
 
+  tags = {
+    Name = lookup(var.components, each.value, null)
+
+
+  }
 }
