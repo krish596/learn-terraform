61e80b6276adc2d433de8d1f56afd32a39193687
Chimtamreddy <kr7348202@gmail.com>
2024-08-08 21:02:33 +0530

    Terraform

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -7,3 +7,11 @@ output "fruits" {
   value = element(var.fruits, 2)
 
 }
+
+variable "fruit_with_stock" {
+  default = [ "apple", "banana", "mango" ]
+}
+
+output "fruit_with_stock" {
+  value = try(var.fruit_with_stock, 3)
+}
