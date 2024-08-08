0d68cadd4a4c137cddeba7ad356c7f0972aa4ac0
Chimtamreddy <kr7348202@gmail.com>
2024-08-08 22:02:49 +0530

    Terraform

---
+++ 06-loops/for-each/main.tf
@@ -0,0 +1,21 @@
+variable "components" {
+  default = {
+    catalogue = { name = "catalogue" }
+    mongodb = { name = "mongodb" }
+
+  }
+}
+
+resource "aws_instance" "instance" {
+
+  count = length(var.components)
+
+  ami  = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0402859183bbbfa7d"]
+
+  tags = {
+    #     Name = var.components[count.index]
+    Name = element(var.components,count.index)
+  }
+}
