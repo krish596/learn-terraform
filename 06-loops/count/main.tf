c72e7604f6a0d51cad07c0b1f27ace8957b72a59
Chimtamreddy <kr7348202@gmail.com>
2024-08-08 22:19:50 +0530

    Terraform

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -1,17 +1,31 @@
+# variable "components" {
+#   default = [ "redis", "catalogue", "frontend", "mongodb" ]
+# }
+#
+# resource "aws_instance" "instance" {
+#
+#   count = length(var.components)
+#
+#   ami  = "ami-0b4f379183e5706b9"
+#   instance_type = "t3.small"
+#   vpc_security_group_ids = ["sg-0402859183bbbfa7d"]
+#
+#   tags = {
+# #     Name = var.components[count.index]
+#     Name = element(var.components,count.index)
+#   }
+# }
+
 variable "components" {
-  default = [ "redis", "catalogue", "frontend", "mongodb" ]
+  default = {
+    catalogue = { name = "catalogue-sg" }
+    mongodb = { name = "mongodb-sg" }
+
+  }
 }
 
-resource "aws_instance" "instance" {
+resource "aws_security_group" "allow_tls" {
+  for_each = var.components
+  name        =  lookup(var.components, each.value["name"], null)
 
-  count = length(var.components)
-
-  ami  = "ami-0b4f379183e5706b9"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-0402859183bbbfa7d"]
-
-  tags = {
-#     Name = var.components[count.index]
-    Name = element(var.components,count.index)
-  }
 }
