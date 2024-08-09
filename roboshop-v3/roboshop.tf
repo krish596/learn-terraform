f29e693c0d225ae472ac4d330c2b85c77d36e698
Chimtamreddy <kr7348202@gmail.com>
2024-08-09 08:41:58 +0530

    Terraform

--- roboshop-v3/roboshop.tf
+++ roboshop-v3/roboshop.tf
@@ -15,16 +15,20 @@ variable "security_group" {
 }
 
 variable "components" {
-  frontend = "{ name = frontend }"
-  mongodb = "{ name = mongodb }"
-  catalogue = "{ name = catalogue }"
-  redis = "{ name = redis }"
-  user = "{ name = user }"
-  cart = "{ name = cart }"
-  mysql = "{ name = mysql }"
-  shipping = "{ name = shipping }"
-  rabbitmq = "{ name = rabbitmq }"
-  payment = "{ name = payment }"
+
+  default = {
+    frontend = "{ name = frontend }"
+    mongodb = "{ name = mongodb }"
+    catalogue = "{ name = catalogue }"
+    redis = "{ name = redis }"
+    user = "{ name = user }"
+    cart = "{ name = cart }"
+    mysql = "{ name = mysql }"
+    shipping = "{ name = shipping }"
+    rabbitmq = "{ name = rabbitmq }"
+    payment = "{ name = payment }"
+  }
+
 
 
 }
