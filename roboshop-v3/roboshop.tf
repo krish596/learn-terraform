ade72c48d57b327696a2504d77eeb2215d73d214
Chimtamreddy <kr7348202@gmail.com>
2024-08-09 09:19:05 +0530

    Terraform

--- roboshop-v3/roboshop.tf
+++ roboshop-v3/roboshop.tf
@@ -40,7 +40,7 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = var.security_group
 
   tags = {
-    Name = lookup(var.components, each.key, null)
+    Name = lookup(var.components, "each.key", null)
   }
 }
 
