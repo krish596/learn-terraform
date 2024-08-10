92fcabdc3c123c575c990e485fb0410204a2bfc9
Chimtamreddy <kr7348202@gmail.com>
2024-08-10 14:44:29 +0530

    Terraform

--- roboshop-v4/module/main.tf
+++ roboshop-v4/module/main.tf
@@ -9,11 +9,8 @@ resource "aws_instance" "instance" {
     Name = var.name
   }
 
-  provisioner "local-exec" {
-    command = "sleep 60"
-  }
-}
 
+}
 
 resource "aws_route53_record" "record" {
 
@@ -22,4 +19,22 @@ resource "aws_route53_record" "record" {
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.private_ip]
+}
+
+resource "null_resource" "ansible" {
+
+  depends_on = [
+    aws_route53_record.record
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/roboshop-ansible
+git pull
+sleep 30
+
+ansible-playbook -i ${var.name}-dev.kr7348202.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+
+EOF
+  }
 }
