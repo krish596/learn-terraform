8c342b932315478e4371ebcc5b99d227b7fccd0a
Chimtamreddy <kr7348202@gmail.com>
2024-08-10 10:42:46 +0530

    Terraform

--- 07-module/local-module/data.tf
+++ 07-module/local-module/data.tf
@@ -1,6 +1,10 @@
-data "aws_ami" "ami" {
+data "aws_ami_ids" "ami" {
   most_recent = true
   name_regex = "Centos-*"
   owners           = ["973714476881"]
 
+}
+
+output "ami" {
+  value = data.aws_ami_ids.ami
 }
