010e0dc5551f00b0817236992d1b9f8faa17e2db
Chimtamreddy <kr7348202@gmail.com>
2024-08-10 12:03:07 +0530

    Terraform

--- 07-module/local-module/data.tf
+++ 07-module/local-module/data.tf
@@ -9,3 +9,11 @@ data "aws_ami" "ami" {
 output "ami" {
   value = data.aws_ami.ami
 }
+
+resource "aws_instance" "instance" {
+
+  ami  = data.aws_ami.ami
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0402859183bbbfa7d"]
+}
+
