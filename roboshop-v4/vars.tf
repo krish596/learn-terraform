3a90a4e256e4cdfbad898a852ec0599033f1031a
Chimtamreddy <kr7348202@gmail.com>
2024-08-10 13:57:38 +0530

    Terraform

---
+++ roboshop-v4/vars.tf
@@ -0,0 +1,18 @@
+variable "zone_id" {
+  default = "Z084007432GC51JNSYQ6A"
+}
+
+
+variable "security_group" {
+  default = ["sg-0402859183bbbfa7d"]
+}
+
+
+variable "components" {
+  default = {
+    frontend = {
+      name = "frontend"
+      instance_type = "t3.small"
+    }
+  }
+}
